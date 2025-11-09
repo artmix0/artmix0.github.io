45f5d3d149687a0105fcb202f1906f30097acffa
artmix0 <gycynyctcycy@gmail.com>
2025-11-09 15:41:07 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+fetch("scraped.json")
+  .then(res => res.json())
+  .then(data => {
+    const lista = document.getElementById("lista");
+    data.forEach(el => {
+      const li = document.createElement("li");
+      li.textContent = el;
+      lista.appendChild(li);
+    });
+  })
+  .catch(err => console.error("Błąd wczytywania danych:", err));
